bf4beb3fc3b1352a3744a6961cee5a6f43b2eebc
tomohiro1024 <gongyezhihong732@gmail.com>
2021-05-08 21:18:09 +0900

    collectionView commit

--- YoutubeApp/ViewController.swift
+++ YoutubeApp/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var videoListCollectionView: UICollectionView!
+    
+    private let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        videoListCollectionView.backgroundColor = .red
+        videoListCollectionView.delegate = self
+        videoListCollectionView.dataSource = self
+        
+        videoListCollectionView.register(UICollectionViewCell,self, forCellWithReuseIdentifier: cellId)
     }
 
+}
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = .green
+        
+        return cell
+    }
+    
+    
 }
 
